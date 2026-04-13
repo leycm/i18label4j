60186ba13cec7e902eae6fd1489dc52e93387004
Lennard <leycm@proton.me>
2026-04-13 21:29:56 +0200

    feat: introduce Label and LabelProvider interfaces for label management These interfaces provide methods for replacing placeholders and serializing/deserializing labels, enhancing the flexibility and functionality of label handling in the application.

---
+++ l4j-api/src/main/java/de/leycm/label4j/Label.java
@@ -0,0 +1,41 @@
+package de.leycm.label4j;
+
+import de.leycm.label4j.excaption.DuplicatePlaceholderException;
+import de.leycm.label4j.placeholder.Placeholder;
+
+import lombok.NonNull;
+
+import java.util.function.Supplier;
+
+public interface Label {
+
+    @NonNull LabelProvider getProvider();
+
+
+    default @NonNull Label replace(final @NonNull String key,
+                           final @NonNull Object value) throws DuplicatePlaceholderException {
+        return replace(key, () -> value);
+    }
+
+    default @NonNull Label replace(final @NonNull String key,
+                           final @NonNull Supplier<Object> supplier) throws DuplicatePlaceholderException {
+        return replace(new Placeholder(key, supplier));
+    }
+
+    @NonNull Label replace(@NonNull Placeholder mapping) throws DuplicatePlaceholderException;
+
+
+    default @NonNull <T> T resolve(final @NonNull Class<T> type) {
+        return getProvider().format(resolve(), type);
+    }
+
+
+    @NonNull String resolve();
+
+    int hashCode();
+
+    boolean equals(@NonNull Label label);
+
+    @NonNull String toString();
+
+}
---
+++ l4j-api/src/main/java/de/leycm/label4j/LabelProvider.java
@@ -0,0 +1,29 @@
+package de.leycm.label4j;
+
+import de.leycm.init4j.instance.Instanceable;
+import de.leycm.label4j.excaption.*;
+import de.leycm.label4j.placeholder.PlaceholderRule;
+
+import lombok.NonNull;
+
+public interface LabelProvider extends Instanceable {
+
+    static @NonNull LabelProvider getInstance() {
+        return Instanceable.getInstance(LabelProvider.class);
+    }
+
+    @NonNull PlaceholderRule getDefaultPlaceholderRule();
+
+    // ==== Serialization ====================================================
+
+    <T> @NonNull T serialize(@NonNull Label label,
+                             @NonNull Class<T> type)
+            throws SerializationException;
+
+    <T> @NonNull Label deserialize(@NonNull T serialized)
+            throws DeserializationException;
+
+    <T> @NonNull T format(@NonNull String input,
+                          @NonNull Class<T> type)
+            throws FormatException;
+}
