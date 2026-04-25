372ce9c98ad52903bbfa5c899926f50ec61afb78
Lennard <leycm@proton.me>
2026-04-25 12:21:01 +0200

    feat(core): add ConstantLabel for static labels

---
+++ l4j-api/src/main/java/de/leycm/label4j/label/ConstantLabel.java
@@ -0,0 +1,106 @@
+package de.leycm.label4j.label;
+
+import de.leycm.label4j.Label;
+import de.leycm.label4j.LabelProvider;
+import de.leycm.label4j.exception.DuplicatePlaceholderException;
+import de.leycm.label4j.localization.Localization;
+import de.leycm.label4j.placeholder.Placeholder;
+import lombok.NonNull;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Set;
+
+@SuppressWarnings("ClassCanBeRecord") // cause: we overwrite record logic
+public class ConstantLabel implements Label, Comparable<ConstantLabel> {
+    private final @NonNull Set<Placeholder> placeholders;
+    private final @NonNull LabelProvider provider;
+    private final @NonNull String key;
+
+    public ConstantLabel(
+            final @NonNull Set<Placeholder> placeholders,
+            final @NonNull LabelProvider provider,
+            final @NonNull String key
+    ) {
+        this.placeholders = placeholders;
+        this.provider = provider;
+        this.key = key;
+
+        if (!Localization.KEY_VALIDATOR.matcher(key).matches()) {
+            throw new IllegalArgumentException(
+                    "Label key contains illegal characters. "
+                            + Localization.KEY_VALIDATOR.pattern()
+                            + ", got: " + key
+            );
+        }
+
+    }
+
+    // ==== Field Methods =====================================================
+
+    public @NonNull String getKey() {
+        return key;
+    }
+
+    @Override
+    public @NonNull LabelProvider getProvider() {
+        return provider;
+    }
+
+    @Override
+    public @UnmodifiableView @NonNull Set<Placeholder> getPlaceholders() {
+        return Collections.unmodifiableSet(placeholders);
+    }
+
+    // ==== Replacement Methods ===============================================
+
+    @Override
+    public @NonNull Label replace(@NonNull Set<@NonNull Placeholder> placeholders) throws DuplicatePlaceholderException {
+        for (final Placeholder placeholder : placeholders) {
+            if (this.placeholders.contains(placeholder)) {
+                throw new DuplicatePlaceholderException(placeholder.key());
+            }
+
+            this.placeholders.add(placeholder);
+        }
+
+        return this;
+    }
+
+    // ==== Resolution Methods ================================================
+
+    @Override
+    public @NonNull Localization localize(@NonNull Locale locale) {
+        return getProvider().localize(Locale.ROOT, key);
+    }
+
+    // ==== Object Methods ====================================================
+
+    @Override
+    public @NonNull String toString() {
+        return Label.class.getSimpleName() + "[const]{" +
+                "key='" + key + '\'' +
+                ", provider=" + provider +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        ConstantLabel that = (ConstantLabel) object;
+        return Objects.equals(key, that.key) && Objects.equals(provider, that.provider);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, provider);
+    }
+
+    @Override
+    public int compareTo(final @NotNull ConstantLabel that) {
+        return this.key.compareTo(that.key);
+    }
+}
